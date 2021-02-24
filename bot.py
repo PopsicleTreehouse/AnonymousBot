3208795694fcbdf0ba72c8b2901fd1676ee9c6f2
Brandon Yao <ahoynodnarb@gmail.com>
2021-02-24 13:14:42 -0800

    Added blacklisting functionality

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from os import getenv
+import os
+import json
 
 bot = commands.Bot(command_prefix="anon-")
 
@@ -23,28 +24,60 @@ async def on_raw_reaction_add(payload):
 
 
 @bot.event
+async def on_message(message):
+    if(message.author == bot.user):
+        await message.add_reaction("\U0001F5D1")
+        return
+    await bot.process_commands(message)
+
+
+@bot.event
 async def on_command_error(ctx, error):
     if(ctx.guild):
-        await ctx.message.delete()
         if(isinstance(error, discord.ext.commands.errors.MissingRequiredArgument)):
             await ctx.message.author.send("Enter a user!")
+            await ctx.message.delete()
+        else:
+            raise error
     else:
         await ctx.send("Type your command into a valid text channel!")
 
 
 @bot.command(name="dm", brief="Anonymously dms user (anon-dm @recipient your sentence)")
-async def dm(ctx, user: discord.User, *, message=None):
+async def dm(ctx, recipient: discord.User, *, message=None):
+    if(recipient.id == ctx.message.author.id):
+        await ctx.message.author.send("You can't anonymously dm yourself!")
     if(ctx.guild):
         await ctx.message.delete()
+        if(os.path.exists("blacklist.json")):
+            with open("blacklist.json") as outfile:
+                if(recipient.id in json.load(outfile)):
+                    await ctx.message.author.send("This person is blacklisted. ")
+                    return
         if(message is None):
-            await ctx.message.author.send("Enter a message to send to " + user.display_name)
+            await ctx.message.author.send("Enter a message to send to " + recipient.display_name)
             return
-        successMsg = await ctx.message.author.send("Successfully sent **" + user.display_name + ":** \"" + message + "\"")
-        await successMsg.add_reaction("\U0001F5D1")
-        msg = await user.send("**An anonymous user sent you this:** "+message)
-        await msg.add_reaction("\U0001F5D1")
+        await ctx.message.author.send("Successfully sent **" + recipient.display_name + ":** \"" + message + "\"")
+        await recipient.send("**An anonymous user sent you this:** "+message)
     else:
         await ctx.send("Type your command into a valid text channel!")
 
+
+@bot.command(name="blacklist", brief="Blacklists yourself from anonymous dms")
+async def blacklist(ctx):
+    if(os.path.isfile("blacklist.json")):
+        with open("blacklist.json") as outfile:
+            blacklist = json.load(outfile)
+            if(ctx.message.author.id in blacklist):
+                blacklist.remove(ctx.message.author.id)
+                await ctx.message.author.send("Successfully removed you from the blacklist")
+            else:
+                blacklist.append(ctx.message.author.id)
+                await ctx.message.author.send("Successfully added you to the blacklist")
+    else:
+        blacklist = [ctx.message.author.id]
+    with open("blacklist.json", 'w') as outfile:
+        json.dump(blacklist, outfile)
+
 load_dotenv()
-bot.run(getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
